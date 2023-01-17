b3487ea05f5071780e0539eef71b36fa41a668b2
tetuyakawarada <tetuya.kawarada@gmail.com>
2023-01-17 22:31:09 +0900

    index,show作成

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Memo;
+use Illuminate\Http\Request;
+
+class MemoController extends Controller
+{
+    public function index()
+    {
+        $memos = Memo::all();
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('memos', App\Http\Controllers\MemoController::class);
+
+// リソースを使用しない場合
+// Route::get('/memos', [App\Http\Controllers\MemoController::class, 'index']);
+// Route::get('/memos/create', [App\Http\Controllers\MemoController::class, 'create']);
+// Route::post('/memos', [App\Http\Controllers\MemoController::class, 'store']);
+// Route::get('/memos/{memo}', [App\Http\Controllers\MemoController::class, 'show']);
+// Route::get('/memos/{memo}/edit', [App\Http\Controllers\MemoController::class, 'edit']);
+// Route::patch('/memos/{memo}', [App\Http\Controllers\MemoController::class, 'update']);
+// Route::delete('/memos/{memo}', [App\Http\Controllers\MemoController::class, 'destroy']);
